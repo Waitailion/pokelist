e3593a2a49a1d8d59481558bd803a56e14ebd575
Waitailion <jfranciscobonifacio@gmail.com>
2021-04-15 15:46:31 -0300

    Updates

---
+++ precache-manifest.a35ae0c50a9f9b923c65259aec0622c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8e5fe9bdce89951d4d70300e53a931",
+    "url": "/pokelist/index.html"
+  },
+  {
+    "revision": "9018cc61c071893c6034",
+    "url": "/pokelist/static/css/main.a7adeacd.chunk.css"
+  },
+  {
+    "revision": "029c1ab6e2c38d8fec6d",
+    "url": "/pokelist/static/js/2.5db98fbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokelist/static/js/2.5db98fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9018cc61c071893c6034",
+    "url": "/pokelist/static/js/main.2de47ec4.chunk.js"
+  },
+  {
+    "revision": "8d0d4c5fb0ff6b064303",
+    "url": "/pokelist/static/js/runtime-main.6448120e.js"
+  },
+  {
+    "revision": "3c3b6cdeb7edc6e6d7bfd3c162a57b8c",
+    "url": "/pokelist/static/media/pokeback3.3c3b6cde.png"
+  },
+  {
+    "revision": "66fb627227d3b397ac512671ec68dd45",
+    "url": "/pokelist/static/media/pokeball.66fb6272.gif"
+  },
+  {
+    "revision": "ac877633ee80e4ed98ebc62224e97adc",
+    "url": "/pokelist/static/media/pokemon.ac877633.png"
+  }
+]);
